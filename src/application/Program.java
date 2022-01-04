14005f5a8310ced8d1fc1aa3806ca4c6be405558
tallesSantos <talles.saint@gmail.com>
2022-01-04 17:58:07 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 import java.sql.Connection;
 import db.DB;
+import model.entities.Department;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Connection conn = null;
+        /*Connection conn = null;
         DB.closeConnection();
         System.out.println("Successfully Connection!!");
+         */
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
